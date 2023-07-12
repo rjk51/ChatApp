a4f41e6ea5c3184ee1685b24b2d4ccc463e2eb3a
rjk51 <ayaanrjk2004@gmail.com>
2023-07-12 22:29:55 +0530

    first commit

---
+++ chat_it/lib/screens/auth.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Auth Screen'),
+      ),
+    );
+  }
+}
